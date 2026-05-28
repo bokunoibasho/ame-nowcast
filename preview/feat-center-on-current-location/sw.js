01e64de28bc198fea1a7f1b9bd33c603c357c865
bokunoibasho <bokunoibasho@users.noreply.github.com>
2026-05-28 18:01:55 +0000

    deploy(preview): feat/center-on-current-location @ 051719608bdeb5c8f6cd106d019d964c56bc1e94 051719608bdeb5c8f6cd106d019d964c56bc1e94

---
+++ preview/feat-center-on-current-location/sw.js
@@ -0,0 +1,46 @@
+// 殻だけオフラインで開けるようにする最低限の Service Worker。
+// 気象庁タイル/JSON や地理院タイルなどの動的・外部リソースはキャッシュせず素通しする。
+const VERSION = 'ame-nowcast-v1';
+const SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon.svg',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/apple-touch-icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(VERSION).then((c) => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== VERSION).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.origin !== location.origin) return;
+
+  e.respondWith(
+    caches.match(req).then((cached) =>
+      cached ||
+      fetch(req)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(VERSION).then((c) => c.put(req, copy));
+          return res;
+        })
+        .catch(() => cached)
+    )
+  );
+});
